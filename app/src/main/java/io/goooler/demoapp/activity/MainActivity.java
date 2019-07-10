7cf69b1331efd82c6a459a73be34ac9c5e7477b1
Goooler <wangzongler@gmail.com>
2019-07-10 22:01:38 +0800

    改用 dataBinding 写法

--- app/src/main/java/io/goooler/demoapp/activity/MainActivity.java
+++ app/src/main/java/io/goooler/demoapp/activity/MainActivity.java
@@ -2,34 +2,39 @@ package io.goooler.demoapp.activity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
+import androidx.databinding.DataBindingUtil;
 
 import io.goooler.demoapp.R;
+import io.goooler.demoapp.async.ClickHandler;
 import io.goooler.demoapp.base.BaseActivity;
 import io.goooler.demoapp.base.BaseApplication;
-import io.goooler.demoapp.util.LogUtil;
+import io.goooler.demoapp.base.BaseBean;
+import io.goooler.demoapp.databinding.ActivityMainBinding;
+import io.goooler.demoapp.util.ToastUtil;
 
-public class MainActivity extends BaseActivity implements View.OnClickListener {
-    private Button button;
+public class MainActivity extends BaseActivity implements ClickHandler {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button = find(R.id.buttonPanel);
-        button.setOnClickListener(this);
-    }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-    @Override
-    public void onClick(View v) {
-        if (v == button) {
-            LogUtil.d("666");
-        }
+        binding.setVm(new BaseBean(1, "button"));
+        binding.setClickHandler(this);
     }
 
     @Override
     protected void onDestroy() {
         BaseApplication.destroyGlobalObject();
         super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonPanel) {
+            ToastUtil.showToast("button clicked");
+        }
     }
 }
