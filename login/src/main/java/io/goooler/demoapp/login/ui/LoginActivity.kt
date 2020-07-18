6370109e408b3590294e3932fb80f4395402589c
Goooler <wangzongler@gmail.com>
2020-07-19 04:59:13 +0800

    重要系统级 bug 记录

--- login/src/main/java/io/goooler/demoapp/login/ui/LoginActivity.kt
+++ login/src/main/java/io/goooler/demoapp/login/ui/LoginActivity.kt
@@ -1,5 +1,6 @@
 package io.goooler.demoapp.login.ui
 
+import android.content.Intent
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
 import io.goooler.demoapp.base.core.BaseActivity
@@ -9,8 +10,14 @@ import io.goooler.demoapp.login.R
 
 @Route(path = RouterPath.LOGIN)
 class LoginActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 首次安装立即打开，后续切后台没有杀死 app 的情况下会触发反复进入 launcherActivity 的 bug
+        if ((intent.flags and Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0) {
+            finish()
+            return
+        }
         setContentView(R.layout.login_activity)
         RouterManager.goMain()
         finish()
