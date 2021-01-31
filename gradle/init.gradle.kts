072982de9926f38fae231ba9620c15e1b965a03d
Goooler <wangzongler@gmail.com>
2021-02-01 01:53:23 +0800

    添加部分仓库和规则判断

--- gradle/init.gradle.kts
+++ gradle/init.gradle.kts
@@ -1,18 +1,20 @@
 allprojects {
-  val aliyunMaven = "https://maven.aliyun.com/repository/public"
+  val aliyunPublic = "https://maven.aliyun.com/repository/public"
+  val aliyunPlugin = "https://maven.aliyun.com/repository/gradle-plugin"
+  val mavenCenter = "https://repo.maven.apache.org/maven2"
+  val jCenter = "https://jcenter.bintray.com"
+  val central = "https://repo1.maven.org/maven2"
+  val pluginPortal = "https://plugins.gradle.org/m2"
   repositories {
     all {
       (this as? MavenArtifactRepository)?.let {
         val url = it.url.toString()
-        if (
-          url.startsWith("https://repo1.maven.org/maven2") ||
-          url.startsWith("https://jcenter.bintray.com/")
-        ) {
-          project.logger.lifecycle("Repository $url replaced by $aliyunMaven.")
+        if (url.startsWith(central) || url.startsWith(mavenCenter) || url.startsWith(jCenter)) {
+          project.logger.lifecycle("Repository $url replaced by $aliyunPublic.")
           remove(it)
         }
       }
     }
-    maven(aliyunMaven)
+    maven(aliyunPublic)
   }
 }
