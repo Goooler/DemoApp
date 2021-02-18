ae15bc13031d4dcbe986d8c709a282169ddf8e9f
goooler <wangzongler@gmail.com>
2021-02-18 22:08:07 +0800

    优化 StatusBarView

--- base/src/main/kotlin/io/goooler/demoapp/base/widget/StatusBarView.kt
+++ base/src/main/kotlin/io/goooler/demoapp/base/widget/StatusBarView.kt
@@ -1,14 +1,10 @@
 package io.goooler.demoapp.base.widget
 
 import android.content.Context
-import android.content.res.Resources
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.Px
 
-/**
- * 为了适配沉浸式状态栏的自定义状态栏，高度适配刘海屏
- */
 class StatusBarView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
 
   override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -17,7 +13,6 @@ class StatusBarView(context: Context, attrs: AttributeSet? = null) : View(contex
 
   @Px
   private fun getStatusBarHeight(): Int {
-    val resources = Resources.getSystem()
     val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
     return resources.getDimensionPixelSize(resourceId)
   }
