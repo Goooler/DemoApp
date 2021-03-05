cf719348f32f08cb6b26352fc5755b9efa594bf0
Goooler <wangzongler@gmail.com>
2021-03-05 11:14:24 +0800

    文档修改

--- compose/src/main/kotlin/io/goooler/demoapp/compose/ComposeActivity.kt
+++ compose/src/main/kotlin/io/goooler/demoapp/compose/ComposeActivity.kt
@@ -2,18 +2,18 @@ package io.goooler.demoapp.compose
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.alibaba.android.arouter.facade.annotation.Route
+import io.goooler.demoapp.base.core.BaseActivity
 import io.goooler.demoapp.common.router.RouterPath
 import io.goooler.demoapp.compose.theme.AndroidComposeDemoTheme
 
 @Route(path = RouterPath.COMPOSE)
-class ComposeActivity : AppCompatActivity() {
+class ComposeActivity : BaseActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
