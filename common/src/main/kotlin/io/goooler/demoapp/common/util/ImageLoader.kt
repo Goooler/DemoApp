f1e220064ea9f3b1b9bb166f5a96082ca83617ae
Goooler <wangzongler@gmail.com>
2021-03-22 14:28:54 +0800

    ImageLoader 内部增加 useCache 参数

--- common/src/main/kotlin/io/goooler/demoapp/common/util/ImageLoader.kt
+++ common/src/main/kotlin/io/goooler/demoapp/common/util/ImageLoader.kt
@@ -50,10 +50,12 @@ object ImageLoader {
   fun load(
     imageView: ImageView,
     data: Any?,
-    @DrawableRes placeholderId: Int
+    @DrawableRes placeholderId: Int,
+    useCache: Boolean = true
   ) {
     imageView.loadAny(data) {
       placeholder(placeholderId)
+      if (useCache.not()) withoutCache()
     }
   }
 
@@ -69,11 +71,13 @@ object ImageLoader {
     imageView: ImageView,
     data: Any?,
     placeholderDrawable: Drawable? = null,
-    errorDrawable: Drawable? = null
+    errorDrawable: Drawable? = null,
+    useCache: Boolean = true
   ) {
     imageView.loadAny(data) {
       placeholder(placeholderDrawable)
       error(errorDrawable)
+      if (useCache.not()) withoutCache()
     }
   }
 
@@ -87,11 +91,13 @@ object ImageLoader {
   fun loadCenterCrop(
     imageView: ImageView,
     data: Any?,
-    @DrawableRes placeholderId: Int
+    @DrawableRes placeholderId: Int,
+    useCache: Boolean = true
   ) {
     imageView.loadAny(data) {
       placeholder(placeholderId)
       scale(Scale.FILL)
+      if (useCache.not()) withoutCache()
     }
   }
 
@@ -107,12 +113,14 @@ object ImageLoader {
     imageView: ImageView,
     data: Any?,
     placeholderDrawable: Drawable? = null,
-    errorDrawable: Drawable? = null
+    errorDrawable: Drawable? = null,
+    useCache: Boolean = true
   ) {
     imageView.loadAny(data) {
       placeholder(placeholderDrawable)
       error(errorDrawable)
       scale(Scale.FILL)
+      if (useCache.not()) withoutCache()
     }
   }
 
@@ -126,11 +134,13 @@ object ImageLoader {
   fun loadCircleCrop(
     imageView: ImageView,
     data: Any?,
-    @DrawableRes placeholderId: Int
+    @DrawableRes placeholderId: Int,
+    useCache: Boolean = true
   ) {
     imageView.loadAny(data) {
       placeholder(placeholderId)
       transformations(CircleCropTransformation())
+      if (useCache.not()) withoutCache()
     }
   }
 
@@ -146,12 +156,14 @@ object ImageLoader {
     imageView: ImageView,
     data: Any?,
     placeholderDrawable: Drawable? = null,
-    errorDrawable: Drawable? = null
+    errorDrawable: Drawable? = null,
+    useCache: Boolean = true
   ) {
     imageView.loadAny(data) {
       placeholder(placeholderDrawable)
       error(errorDrawable)
       transformations(CircleCropTransformation())
+      if (useCache.not()) withoutCache()
     }
   }
 
@@ -167,12 +179,14 @@ object ImageLoader {
     imageView: ImageView,
     data: Any?,
     @Px radius: Int,
-    @DrawableRes placeholderId: Int
+    @DrawableRes placeholderId: Int,
+    useCache: Boolean = true
   ) {
     val radiusF = radius.toFloat()
     imageView.loadAny(data) {
       placeholder(placeholderId)
       transformations(RoundedCornersTransformation(radiusF, radiusF, radiusF, radiusF))
+      if (useCache.not()) withoutCache()
     }
   }
 
@@ -190,67 +204,38 @@ object ImageLoader {
     data: Any?,
     @Px radius: Int,
     placeholderDrawable: Drawable? = null,
-    errorDrawable: Drawable? = null
+    errorDrawable: Drawable? = null,
+    useCache: Boolean = true
   ) {
     val radiusF = radius.toFloat()
     imageView.loadAny(data) {
       placeholder(placeholderDrawable)
       error(errorDrawable)
       transformations(RoundedCornersTransformation(radiusF, radiusF, radiusF, radiusF))
-    }
-  }
-
-  /**
-   * Load image without cache.
-   *
-   * @param imageView     the image view
-   * @param data          the data
-   * @param placeholderId the placeholder id
-   */
-  fun loadWithoutCache(
-    imageView: ImageView,
-    data: Any?,
-    @DrawableRes placeholderId: Int
-  ) {
-    imageView.loadAny(data) {
-      placeholder(placeholderId)
-      memoryCachePolicy(CachePolicy.DISABLED)
-      diskCachePolicy(CachePolicy.DISABLED)
-    }
-  }
-
-  /**
-   * Load image without cache.
-   *
-   * @param imageView           the image view
-   * @param data                the data
-   * @param placeholderDrawable the placeholder drawable
-   * @param errorDrawable       the error drawable
-   */
-  fun loadWithoutCache(
-    imageView: ImageView,
-    data: Any?,
-    placeholderDrawable: Drawable? = null,
-    errorDrawable: Drawable? = null
-  ) {
-    imageView.loadAny(data) {
-      placeholder(placeholderDrawable)
-      error(errorDrawable)
-      memoryCachePolicy(CachePolicy.DISABLED)
-      diskCachePolicy(CachePolicy.DISABLED)
+      if (useCache.not()) withoutCache()
     }
   }
 
   suspend fun getDrawable(
     uri: String?,
     context: Context? = null,
+    useCache: Boolean = true,
     builder: ImageRequest.Builder.() -> Unit = {}
   ): Drawable? {
     val request = ImageRequest.Builder(context ?: mContext)
       .data(uri)
-      .apply(builder)
+      .apply {
+        if (useCache.not()) withoutCache()
+        builder()
+      }
       .build()
     return Coil.execute(request).drawable
+  }
+
+  private fun ImageRequest.Builder.withoutCache(): ImageRequest.Builder {
+    memoryCachePolicy(CachePolicy.DISABLED)
+    diskCachePolicy(CachePolicy.DISABLED)
+    return this
   }
 }
 
