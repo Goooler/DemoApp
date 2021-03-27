8e8b21d6d5773ee9f47d50bc72fb9e51540ef1b2
Goooler <wangzongler@gmail.com>
2021-03-27 19:00:01 +0800

    JsonUtil 优化

---
+++ test/src/main/kotlin/io/goooler/demoapp/test/util/JsonConverter.kt
@@ -0,0 +1,78 @@
+@file:Suppress("unused")
+
+package io.goooler.demoapp.test.util
+
+import com.alibaba.fastjson.JSONObject
+import com.alibaba.fastjson.TypeReference
+import com.google.gson.Gson
+import java.lang.reflect.Type
+
+interface JsonConverter {
+
+  fun <T> fromJson(json: String, classOfT: Class<T>): T?
+
+  fun <T> fromJson(json: String, typeOfT: Type): T?
+
+  fun toJson(src: Any): String?
+
+  interface GsonUtil : JsonConverter {
+
+    companion object : GsonUtil {
+      private val gson = Gson()
+
+      override fun <T> fromJson(json: String, classOfT: Class<T>): T? = try {
+        gson.fromJson(json, classOfT)
+      } catch (e: Exception) {
+        e.printStackTrace()
+        null
+      }
+
+      override fun <T> fromJson(json: String, typeOfT: Type): T? = try {
+        gson.fromJson(json, typeOfT)
+      } catch (e: Exception) {
+        e.printStackTrace()
+        null
+      }
+
+      override fun toJson(src: Any): String? = try {
+        gson.toJson(src)
+      } catch (e: Exception) {
+        e.printStackTrace()
+        null
+      }
+    }
+  }
+
+  interface FastJsonUtil : JsonConverter {
+
+    companion object : FastJsonUtil {
+      override fun <T> fromJson(json: String, classOfT: Class<T>): T? = try {
+        JSONObject.parseObject(json, classOfT)
+      } catch (e: Exception) {
+        e.printStackTrace()
+        null
+      }
+
+      override fun <T> fromJson(json: String, typeOfT: Type): T? = try {
+        JSONObject.parseObject(json, typeOfT)
+      } catch (e: Exception) {
+        e.printStackTrace()
+        null
+      }
+
+      fun <T> fromJson(json: String, typeRef: TypeReference<T>): T? = try {
+        JSONObject.parseObject(json, typeRef)
+      } catch (e: Exception) {
+        e.printStackTrace()
+        null
+      }
+
+      override fun toJson(src: Any): String? = try {
+        JSONObject.toJSONString(src)
+      } catch (e: Exception) {
+        e.printStackTrace()
+        null
+      }
+    }
+  }
+}
