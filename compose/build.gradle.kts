94ff6afec5b04048a0db8d6515235317631ea49b
Goooler <wangzongler@gmail.com>
2021-04-06 15:32:34 +0800

    compose 编译参数修改

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -1,13 +1,6 @@
 setupLib(Module.Compose) {
   buildFeatures.compose = true
   composeOptions.kotlinCompilerExtensionVersion = composeVersion
-  kotlinOptions {
-    freeCompilerArgs = freeCompilerArgs + listOf(
-      "-Xallow-jvm-ir-dependencies",
-      "-P",
-      "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
-    )
-  }
 }
 
 dependencies {
