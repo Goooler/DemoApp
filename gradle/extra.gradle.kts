bee2c0626f2b3891498efba67bef34a03c369e65
Goooler <wangzongler@gmail.com>
2021-04-06 10:22:55 +0800

    AGP 7.0.0-alpha13

--- gradle/extra.gradle.kts
+++ gradle/extra.gradle.kts
@@ -2,7 +2,7 @@ import java.nio.charset.StandardCharsets
 import java.util.concurrent.TimeUnit
 
 rootProject.extra.run {
-  set("androidGradlePlugin", "com.android.tools.build:gradle:7.0.0-alpha12")
+  set("androidGradlePlugin", "com.android.tools.build:gradle:7.0.0-alpha13")
   set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
 }
 
