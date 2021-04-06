fc6ff079dcd248e865d4b07850cbd62cb4141922
Goooler <wangzongler@gmail.com>
2021-04-06 16:27:02 +0800

    添加创建 shortcut 方法

--- main/src/main/kotlin/io/goooler/demoapp/main/ui/fragment/MainHomeFragment.kt
+++ main/src/main/kotlin/io/goooler/demoapp/main/ui/fragment/MainHomeFragment.kt
@@ -1,6 +1,14 @@
 package io.goooler.demoapp.main.ui.fragment
 
+import android.annotation.TargetApi
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
+import android.os.Build
 import android.view.View
+import androidx.core.content.getSystemService
 import dagger.hilt.android.AndroidEntryPoint
 import io.goooler.demoapp.base.util.unsafeLazy
 import io.goooler.demoapp.common.base.BaseThemeLazyFragment
@@ -9,6 +17,7 @@ import io.goooler.demoapp.common.ui.FullScreenDialogFragment
 import io.goooler.demoapp.common.util.getViewModel
 import io.goooler.demoapp.main.R
 import io.goooler.demoapp.main.databinding.MainHomeFragmentBinding
+import io.goooler.demoapp.main.ui.AudioPlayActivity
 import io.goooler.demoapp.main.vm.MainHomeViewModel
 import java.util.concurrent.CancellationException
 
@@ -40,6 +49,7 @@ class MainHomeFragment :
       binding.bt5 -> RouterManager.goWidget()
       binding.bt6 -> FullScreenDialogFragment.show(childFragmentManager)
       binding.bt7 -> RouterManager.goAudioPlay()
+      binding.bt8 -> createShortcut(requireContext())
     }
   }
 
@@ -53,7 +63,32 @@ class MainHomeFragment :
     }
   }
 
+  @TargetApi(Build.VERSION_CODES.N_MR1)
+  private fun createShortcut(context: Context) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+      context.getSystemService<ShortcutManager>()?.let { sm ->
+        if (sm.dynamicShortcuts.any { it.id == SHORTCUT_ID }) {
+          return
+        }
+        val intent = Intent(context, AudioPlayActivity::class.java)
+          .setAction(Intent.ACTION_VIEW)
+        val shortcut = ShortcutInfo.Builder(context, SHORTCUT_ID)
+          .setShortLabel("Start audio play")
+          .setLongLabel("Start audio play")
+          .setIcon(Icon.createWithResource(context, R.drawable.common_ic_kt_red))
+          .setIntent(intent)
+          .build()
+        try {
+          sm.addDynamicShortcuts(listOf(shortcut))
+        } catch (_: Exception) {
+        }
+      }
+    }
+  }
+
   companion object {
+    private const val SHORTCUT_ID = "audioPlayShortcutId"
+
     fun newInstance(): MainHomeFragment = MainHomeFragment()
   }
 }
