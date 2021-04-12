a3ad896a5260e92a3f088f34bb565a3150575bad
Goooler <wangzongler@gmail.com>
2021-04-12 10:43:40 +0800

    init.gradle.kts 优化

--- gradle/init.gradle.kts
+++ gradle/init.gradle.kts
@@ -13,8 +13,15 @@ allprojects {
           project.logger.lifecycle("Repository $url replaced by $aliyunPublic.")
           remove(it)
         }
+        if (url.startsWith(pluginPortal)) {
+          project.logger.lifecycle("Repository $url replaced by $aliyunPlugin.")
+          remove(it)
+        }
       }
+      // 适配 Gradle 7.0，允许不安全的协议
+      (this as? UrlArtifactRepository)?.setAllowInsecureProtocol(true)
     }
     maven(aliyunPublic)
+    maven(aliyunPlugin)
   }
 }
