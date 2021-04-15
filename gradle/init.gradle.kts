9680580fe5ceff07adf780ac3807060286427d7d
Goooler <wangzongler@gmail.com>
2021-04-16 00:32:34 +0800

    添加 dependencyUpdates.gradle

--- gradle/init.gradle.kts
+++ gradle/init.gradle.kts
@@ -1,10 +1,12 @@
 allprojects {
   val aliyunPublic = "https://maven.aliyun.com/repository/public"
   val aliyunPlugin = "https://maven.aliyun.com/repository/gradle-plugin"
+
   val mavenCenter = "https://repo.maven.apache.org/maven2"
   val jCenter = "https://jcenter.bintray.com"
   val central = "https://repo1.maven.org/maven2"
   val pluginPortal = "https://plugins.gradle.org/m2"
+
   repositories {
     all {
       (this as? MavenArtifactRepository)?.let {
