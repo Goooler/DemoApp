0520ea316b496181061c09b8ef984c5e2ec07e42
Goooler <wangzongler@gmail.com>
2021-04-26 16:41:05 +0800

    ResponseException 优化

--- test/src/main/kotlin/io/goooler/demoapp/test/network/exception/ResponseException.kt
+++ test/src/main/kotlin/io/goooler/demoapp/test/network/exception/ResponseException.kt
@@ -3,21 +3,17 @@ package io.goooler.demoapp.test.network.exception
 import retrofit2.HttpException
 import java.io.IOException
 
-class ResponseException(val error: String, throwable: Throwable?) : Exception() {
+class ResponseException(message: String?, throwable: Throwable?) : Exception(message, throwable)
 
-  init {
-    initCause(throwable)
-  }
-}
-
-fun Throwable.toResponseException(): ResponseException = when (this) {
+fun Throwable?.toResponseException(): ResponseException = when (this) {
   is ResponseException -> this
   is HttpException -> {
-    val error = if (code() == 401) "auth failed" else "connect failed"
-    ResponseException(error, this)
+    val message = if (code() == 401) "auth failed" else "connect failed"
+    ResponseException(message, this)
   }
   is IOException -> ResponseException("timeout", this)
   else -> ResponseException("unknown error", this)
 }
 
-fun String.toResponseException(t: Throwable? = null): ResponseException = ResponseException(this, t)
+fun String?.toResponseException(t: Throwable? = null): ResponseException =
+  ResponseException(this, t)
