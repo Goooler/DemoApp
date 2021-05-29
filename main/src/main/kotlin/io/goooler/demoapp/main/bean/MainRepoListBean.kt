7929d91d2b766a7fe543c30342c7c7ddd2002903
Goooler <wangzongler@gmail.com>
2021-05-29 11:16:56 +0800

    MainRepoListBean 的 room 注解修改

--- main/src/main/kotlin/io/goooler/demoapp/main/bean/MainRepoListBean.kt
+++ main/src/main/kotlin/io/goooler/demoapp/main/bean/MainRepoListBean.kt
@@ -9,7 +9,7 @@ import com.squareup.moshi.JsonClass
 import io.goooler.demoapp.common.network.BaseResponse
 
 @JsonClass(generateAdapter = true)
-@Entity(tableName = "main_repo_list", ignoredColumns = ["status", "message", "code", "count"])
+@Entity(tableName = "main_repo_list", ignoredColumns = ["message", "code"])
 class MainRepoListBean : BaseResponse() {
 
   @PrimaryKey
