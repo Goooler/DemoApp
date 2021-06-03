f74039dc11226ac1df132136cb8d3e9e078cd21e
Goooler <wangzongler@gmail.com>
2021-06-03 17:01:05 +0800

    无用依赖删除

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,19 +1,16 @@
 @file:Suppress("SpellCheckingInspection")
 
 private const val lifecycleVersion = "2.3.1"
-private const val navigationVersion = "2.3.1"
 private const val okHttpVersion = "4.9.1"
 private const val retrofitVersion = "2.9.0"
 private const val arouterVersion = "1.5.1"
 private const val moshiVersion = "1.12.0"
 private const val smartRefreshLayoutVersion = "2.0.3"
 private const val glideVersion = "4.12.0"
-private const val objectBoxVersion = "2.9.1"
 private const val protocVersion = "3.15.4"
 private const val coilVersion = "1.2.1"
 private const val roomVersion = "2.3.0"
 private const val hiltVersion = "2.36"
-private const val dataStoreVersion = "1.0.0-alpha07"
 private const val doraemonKitVersion = "3.3.5"
 private const val chuckerVersion = "3.4.0"
 private const val accompanistVersion = "0.6.2"
@@ -27,7 +24,6 @@ object Plugins {
   const val androidApplication = "com.android.application"
   const val hilt = "dagger.hilt.android.plugin"
   const val arouter = "com.alibaba.arouter"
-  const val protobuf = "com.google.protobuf"
   const val kotlinter = "org.jmailen.kotlinter"
   const val detekt = "io.gitlab.arturbosch.detekt"
   const val picCompress = "io.goooler.android.plugin.pic-compress"
@@ -37,33 +33,21 @@ object Plugins {
 object Libs {
   const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0"
   const val core = "androidx.core:core-ktx:1.5.0"
-  const val desugar = "com.android.tools:desugar_jdk_libs:1.1.5"
   const val appCompat = "androidx.appcompat:appcompat:1.3.0"
   const val annotation = "androidx.annotation:annotation:1.2.0"
   const val activity = "androidx.activity:activity-ktx:1.2.3"
   const val fragment = "androidx.fragment:fragment-ktx:1.3.4"
-  const val legacySupport = "androidx.legacy:legacy-support-v4:1.0.0"
-  const val preference = "androidx.preference:preference:1.1.1"
-  const val palette = "androidx.palette:palette-ktx:1.0.0"
   const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.4"
   const val coordinatorLayout = "androidx.coordinatorlayout:coordinatorlayout:1.1.0"
   const val material = "com.google.android.material:material:1.3.0"
   const val recyclerView = "androidx.recyclerview:recyclerview:1.2.1"
-  const val workManager = "androidx.work:work-runtime-ktx:2.4.0"
-  const val flexBox = "com.google.android:flexbox:2.0.1"
   const val cardView = "androidx.cardview:cardview:1.0.0"
   const val collection = "androidx.collection:collection-ktx:1.1.0"
-  const val mpChart = "com.github.PhilJay:MPAndroidChart:v3.1.0"
-  const val lottie = "com.airbnb.android:lottie:3.5.0"
   const val fastjson = "com.alibaba:fastjson:1.1.72.android"
   const val arouter = "com.alibaba:arouter-api:$arouterVersion"
   const val luban = "top.zibin:Luban:1.1.8"
   const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.7"
-  const val mmkv = "com.tencent:mmkv-static:1.2.8"
   const val protoc = "com.google.protobuf:protoc:$protocVersion"
-  const val protobufLite = "com.google.protobuf:protobuf-javalite:$protocVersion"
-  const val tbs = "com.tencent.tbs.tbssdk:sdk:43938"
-  const val baseRvHelper = "com.github.CymChad:BaseRecyclerViewAdapterHelper:3.0.6"
   const val flycoTabLayout = "com.flyco.tablayout:FlycoTabLayout_Lib:2.2.0"
   const val utils = "com.blankj:utilcodex:1.30.6"
   const val okHttp = "com.squareup.okhttp3:okhttp:$okHttpVersion"
@@ -80,13 +64,11 @@ object Libs {
   const val chuckerDebug = "com.github.chuckerteam.chucker:library:$chuckerVersion"
   const val chuckerRelease = "com.github.chuckerteam.chucker:library-no-op:$chuckerVersion"
 
-  const val protobufPlugin = "com.google.protobuf:protobuf-gradle-plugin:0.8.15"
   const val arouterPlugin = "com.alibaba:arouter-register:1.0.2"
   const val hiltPlugin = "com.google.dagger:hilt-android-gradle-plugin:$hiltVersion"
   const val kotlinterPlugin = "org.jmailen.gradle:kotlinter-gradle:3.4.4"
   const val detektPlugin = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.17.1"
 
-  const val objectBoxCompiler = "io.objectbox:objectbox-processor:$objectBoxVersion"
   const val roomCompiler = "androidx.room:room-compiler:$roomVersion"
   const val arouterCompiler = "com.alibaba:arouter-compiler:$arouterVersion"
   const val moshiCompiler = "com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion"
@@ -126,16 +108,6 @@ object Libs {
     "com.google.dagger:hilt-android:$hiltVersion",
     "androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03"
   )
-  val dataStore = arrayOf(
-    "androidx.datastore:datastore-preferences:$dataStoreVersion",
-    "androidx.datastore:datastore-core:$dataStoreVersion"
-  )
-  val navigation = arrayOf(
-    "androidx.navigation:navigation-ui-ktx:$navigationVersion",
-    "androidx.navigation:navigation-fragment-ktx:$navigationVersion",
-    "androidx.navigation:navigation-common-ktx:$navigationVersion",
-    "androidx.navigation:navigation-runtime-ktx:$navigationVersion"
-  )
   val smartRefreshLayout = arrayOf(
     "com.scwang.smart:refresh-layout-kernel:$smartRefreshLayoutVersion",
     "com.scwang.smart:refresh-header-classics:$smartRefreshLayoutVersion",
@@ -159,10 +131,6 @@ object Libs {
     "io.coil-kt:coil-gif:$coilVersion",
     "io.coil-kt:coil-svg:$coilVersion",
     "com.github.Commit451.coil-transformations:transformations:1.0.0"
-  )
-  val objectBox = arrayOf(
-    "io.objectbox:objectbox-android:$objectBoxVersion",
-    "io.objectbox:objectbox-kotlin:$objectBoxVersion"
   )
   val rx = arrayOf(
     "io.reactivex.rxjava3:rxjava:3.0.13",
