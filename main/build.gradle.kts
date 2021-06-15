0aa892dd7062209e76d8a30a91eb126ee1d10716
Goooler <wangzongler@gmail.com>
2021-06-15 13:53:16 +0800

    main 模块添加 testInstrumentationRunner

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -1,4 +1,6 @@
 setupLib(Module.Main) {
+  defaultConfig.testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
   sourceSets["main"].res.srcDirs(
     "src/main/res/core",
     "src/main/res/other"
