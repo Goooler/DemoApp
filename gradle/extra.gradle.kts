b20dae637781ce5cf91bb250d2a7db8a1b266975
Goooler <wangzongler@gmail.com>
2021-07-30 00:56:51 +0800

    依赖升级

--- gradle/extra.gradle.kts
+++ gradle/extra.gradle.kts
@@ -2,7 +2,7 @@ import java.nio.charset.StandardCharsets
 import java.util.concurrent.TimeUnit
 
 rootProject.extra.run {
-  set("androidPlugin", "com.android.tools.build:gradle:4.2.2")
+  set("androidPlugin", "com.android.tools.build:gradle:7.0.0")
   set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
 }
 
