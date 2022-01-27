93c84cd3e2fae65f64ad3ac63e8710ab0575905a
Goooler <wangzongler@gmail.com>
2022-01-27 15:49:52 +0800

    简化 ToastUtil

--- base/src/main/kotlin/io/goooler/demoapp/base/util/ToastUtil.kt
+++ base/src/main/kotlin/io/goooler/demoapp/base/util/ToastUtil.kt
@@ -14,18 +14,13 @@ import java.lang.ref.WeakReference
 object ToastUtil {
 
   private val handler = Handler(Looper.getMainLooper())
-  private var toast: WeakReference<Toast?>? = null
+  private var toast: WeakReference<Toast>? = null
 
   @AnyThread
   fun show(context: Context, @StringRes strResId: Int) {
     show(context, context.getString(strResId))
   }
 
-  /**
-   * 可在子线程使用的 toast
-   *
-   * @param text string 文本
-   */
   @AnyThread
   fun show(context: Context, text: String) {
     if (Looper.getMainLooper() == Looper.myLooper()) {
@@ -36,17 +31,8 @@ object ToastUtil {
   }
 
   @MainThread
-  fun showInMain(context: Context, @StringRes strResId: Int) {
-    showInMain(context, context.getString(strResId))
-  }
-
-  /**
-   * 只在主线程调用，真正实现 toast 的方法
-   */
-  @MainThread
   @Synchronized
   fun showInMain(context: Context, text: String) {
-    // 把上一条先置空，再显示下一条
     toast?.get()?.cancel()
     toast = null
     Toast.makeText(context, text, Toast.LENGTH_SHORT).also {
