2945a873eee2f7c1565efbbc6c9a1602d11e2e7b
Goooler <wangzongler@gmail.com>
2022-02-06 18:19:41 +0800

    Downgrade AGP

--- gradle/extra.gradle.kts
+++ gradle/extra.gradle.kts
@@ -1,7 +1,7 @@
 val kotlinVersion = "1.6.10"
 
 rootProject.extra.run {
-  set("androidPlugin", "com.android.tools.build:gradle:7.3.0-alpha01")
+  set("androidPlugin", "com.android.tools.build:gradle:7.1.1")
   set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 }
 
