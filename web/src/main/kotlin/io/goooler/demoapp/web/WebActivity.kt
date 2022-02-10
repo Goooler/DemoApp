9ec8beab8d72d7a4bfbc11e5e2d3e51300e806fe
Goooler <wangzongler@gmail.com>
2022-02-10 11:23:06 +0800

    Add BlankFragment for tests

--- web/src/main/kotlin/io/goooler/demoapp/web/WebActivity.kt
+++ web/src/main/kotlin/io/goooler/demoapp/web/WebActivity.kt
@@ -1,73 +1,34 @@
 package io.goooler.demoapp.web
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import androidx.browser.customtabs.CustomTabsIntent
-import androidx.core.net.toUri
+import androidx.lifecycle.lifecycleScope
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.blankj.utilcode.util.BarUtils
 import io.goooler.demoapp.base.util.addFragment
+import io.goooler.demoapp.base.util.replaceFragment
 import io.goooler.demoapp.common.base.binding.BaseBindingActivity
-import io.goooler.demoapp.common.router.RouterManager
 import io.goooler.demoapp.common.router.RouterPath
 import io.goooler.demoapp.web.databinding.WebActivityBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Route(path = RouterPath.WEB)
 class WebActivity : BaseBindingActivity<WebActivityBinding>() {
 
-  private var webFragment: WebFragment? = null
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     BarUtils.setStatusBarLightMode(this, true)
-    binding.listener = listener
-    intent.extras?.getString(RouterManager.PARAMS)?.let { url ->
-      if (intent.action == RouterManager.USE_CHROME) {
-        CustomTabsIntent.Builder()
-          .build()
-          .launchUrl(this, url.toUri())
-        finish()
-      } else {
-        webFragment = WebFragment(url).also {
-          it.onEventListener = listener
-          addFragment(it, R.id.fragment_container)
-        }
-      }
-    }
-  }
 
-  override fun onBackPressed() {
-    if (webFragment?.goBack() == false) {
-      super.onBackPressed()
-    }
-  }
+    val blankFragment = BlankFragment()
 
-  private val listener = object : View.OnClickListener, WebFragment.OnEventListener {
-    override fun onClick(v: View) {
-      when (v) {
-        binding.layoutTitle.ivLeft -> {
-          finish()
-        }
-        binding.layoutTitle.ivRight -> {
-          val intent = Intent().setAction(Intent.ACTION_SEND)
-            .putExtra(Intent.EXTRA_TEXT, webFragment?.url)
-            .setType("text/plain")
-          startActivity(Intent.createChooser(intent, null))
-        }
-      }
-    }
+    addFragment(blankFragment, R.id.fragment_container)
 
-    override fun onReceivedTitle(title: String) {
-      binding.layoutTitle.title = title
-    }
-
-    override fun onProgressChanged(i: Int) {
-      binding.progressBar.visibility = if (i >= 100) {
-        View.GONE
-      } else {
-        binding.progressBar.progress = i
-        View.VISIBLE
+    lifecycleScope.launch(Dispatchers.IO) {
+      delay(1000)
+      withContext(Dispatchers.Main) {
+        replaceFragment(WebFragment("bilibili.com"), R.id.fragment_container, true)
       }
     }
   }
