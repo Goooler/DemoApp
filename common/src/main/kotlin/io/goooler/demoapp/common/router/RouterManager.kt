857802778fe3d4d9d8b9d612015b1b9746b32078
Goooler <wangzongler@gmail.com>
2022-02-11 11:35:32 +0800

    Make RouterManager as a interface

--- common/src/main/kotlin/io/goooler/demoapp/common/router/RouterManager.kt
+++ common/src/main/kotlin/io/goooler/demoapp/common/router/RouterManager.kt
@@ -1,53 +1,45 @@
 package io.goooler.demoapp.common.router
 
-import android.content.Intent
-import androidx.core.net.toUri
-import com.alibaba.android.arouter.facade.Postcard
-import com.alibaba.android.arouter.launcher.ARouter
+import javax.inject.Inject
 
-@Suppress("unused", "MemberVisibilityCanBePrivate")
-object RouterManager {
-  const val TARGET = "target"
-  const val PARAMS = "params"
-  const val RE_LOGIN = "reLogin"
-  const val USE_CHROME = "useChrome"
+interface RouterManager {
 
-  fun go(url: String) {
-    url.toUri().path?.let {
-      buildPostcard(it).navigation()
+  fun go(url: String)
+
+  fun goLogin(isReLogin: Boolean)
+
+  fun goMain()
+
+  fun goAudioPlay()
+
+  fun goWeb(url: String, useChrome: Boolean = false)
+
+  companion object : RouterManager {
+    const val PARAMS = "params"
+    const val RE_LOGIN = "reLogin"
+    const val USE_CHROME = "useChrome"
+
+    @Inject
+    lateinit var impl: RouterManager
+
+    override fun go(url: String) {
+      impl.go(url)
+    }
+
+    override fun goLogin(isReLogin: Boolean) {
+      impl.goLogin(isReLogin)
+    }
+
+    override fun goMain() {
+      impl.goMain()
+    }
+
+    override fun goAudioPlay() {
+      impl.goAudioPlay()
+    }
+
+    override fun goWeb(url: String, useChrome: Boolean) {
+      impl.goWeb(url, useChrome)
     }
   }
-
-  fun goLogin(isReLogin: Boolean) {
-    val action = if (isReLogin) RE_LOGIN else null
-    buildPostcard(RouterPath.LOGIN)
-      .withAction(action)
-      .withFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-      .navigation()
-  }
-
-  fun goMain() {
-    buildPostcard(RouterPath.MAIN)
-      .navigation()
-  }
-
-  fun goMap() {
-    buildPostcard(RouterPath.MAP)
-      .navigation()
-  }
-
-  fun goAudioPlay() {
-    buildPostcard(RouterPath.AUDIO_PLAY)
-      .navigation()
-  }
-
-  fun goWeb(url: String, useChrome: Boolean = false) {
-    val action = if (useChrome) USE_CHROME else null
-    buildPostcard(RouterPath.WEB)
-      .withAction(action)
-      .withString(PARAMS, url)
-      .navigation()
-  }
-
-  private fun buildPostcard(path: String): Postcard = ARouter.getInstance().build(path)
 }
--- common/src/main/kotlin/io/goooler/demoapp/common/router/RouterPath.kt
+++ common/src/main/kotlin/io/goooler/demoapp/common/router/RouterPath.kt
@@ -3,7 +3,6 @@ package io.goooler.demoapp.common.router
 object RouterPath {
   const val LOGIN = "/login/"
   const val MAIN = "/main/"
-  const val MAP = "/map/"
   const val WEB = "/web/"
   const val AUDIO_PLAY = "/audioPlay/"
 }
