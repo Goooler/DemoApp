850312765e72c5d6a7647c58ab2698e870145d0e
Goooler <wangzongler@gmail.com>
2022-02-28 11:38:33 +0800

    Add JsonUtil to JsonUtil

--- common/src/main/kotlin/io/goooler/demoapp/common/util/JsonUtil.kt
+++ common/src/main/kotlin/io/goooler/demoapp/common/util/JsonUtil.kt
@@ -4,45 +4,38 @@ package io.goooler.demoapp.common.util
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
-import java.lang.reflect.Type
 
 object JsonUtil {
+  @PublishedApi
   internal val moshi: Moshi = Moshi.Builder().build()
 
-  fun <T> fromJson(json: String, clazz: Class<T>): T? = try {
-    moshi.adapter(clazz).fromJson(json)
+  inline fun <reified T> fromJson(json: String): T? = try {
+    moshi.adapter(T::class.java).fromJson(json)
   } catch (e: Exception) {
     e.printStackTrace()
     null
   }
 
-  fun <T> fromJson(json: String, typeOfT: Type): T? = try {
-    moshi.adapter<T>(typeOfT).fromJson(json)
+  inline fun <reified T> fromJson(
+    json: String,
+    rawType: Class<*>,
+    vararg typeArguments: Class<*>
+  ): T? = try {
+    moshi.adapter<T>(Types.newParameterizedType(rawType, *typeArguments)).fromJson(json)
   } catch (e: Exception) {
     e.printStackTrace()
     null
   }
 
-  fun <T> fromJson(json: String, rawType: Class<*>, vararg typeArguments: Class<*>): T? = try {
-    fromJson(json, Types.newParameterizedType(rawType, *typeArguments))
+  inline fun <reified T> toJson(o: T?): String? = try {
+    moshi.adapter(T::class.java).toJson(o)
   } catch (e: Exception) {
     e.printStackTrace()
     null
   }
-
-  fun <T> toJson(o: T?, clazz: Class<T>): String? = try {
-    moshi.adapter(clazz).toJson(o)
-  } catch (e: Exception) {
-    e.printStackTrace()
-    null
-  }
-
-  inline fun <reified T> toJson(o: T?): String? = toJson(o, T::class.java)
 }
 
 inline fun <reified T> String.fromJson(): T? = JsonUtil.fromJson(this, T::class.java)
-
-inline fun <reified T> String.fromJson(typeOfT: Type): T? = JsonUtil.fromJson(this, typeOfT)
 
 inline fun <reified T> String.fromJson(rawType: Class<*>, vararg typeArguments: Class<*>): T? =
   JsonUtil.fromJson(this, rawType, *typeArguments)
