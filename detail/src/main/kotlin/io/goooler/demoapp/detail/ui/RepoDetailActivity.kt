a6474d16e9b0eb6f95c11607537753aeadbb8346
Goooler <wangzongler@gmail.com>
2022-03-16 14:53:42 +0800

    Add HelloScreen

--- detail/src/main/kotlin/io/goooler/demoapp/detail/ui/RepoDetailActivity.kt
+++ detail/src/main/kotlin/io/goooler/demoapp/detail/ui/RepoDetailActivity.kt
@@ -1,11 +1,24 @@
 package io.goooler.demoapp.detail.ui
 
 import android.os.Bundle
-import io.goooler.demoapp.common.base.theme.BaseThemeActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import io.goooler.demoapp.base.core.BaseActivity
 
-class RepoDetailActivity : BaseThemeActivity() {
+class RepoDetailActivity : BaseActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    setContent {
+      HelloScreen()
+    }
   }
+}
+
+@Preview
+@Composable
+fun HelloScreen() {
+  Text(text = "Hello Compose")
 }
