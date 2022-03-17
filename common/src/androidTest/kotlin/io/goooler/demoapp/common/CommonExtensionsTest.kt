58dc7ec0e8b440d3d271aeb3863bd86f14d179ef
Goooler <wangzongler@gmail.com>
2022-03-17 18:12:45 +0800

    Add CommonExtensionsTest

---
+++ common/src/androidTest/kotlin/io/goooler/demoapp/common/CommonExtensionsTest.kt
@@ -0,0 +1,14 @@
+package io.goooler.demoapp.common
+
+import io.goooler.demoapp.common.util.getQuantityString
+import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test
+
+class CommonExtensionsTest {
+
+  @Test
+  fun intGetQuantityStringTest() {
+    assertEquals(R.plurals.number_of_songs_available.getQuantityString(1), "1 song found.")
+    assertEquals(R.plurals.number_of_songs_available.getQuantityString(2), "2 songs found.")
+  }
+}
