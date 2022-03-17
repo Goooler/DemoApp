c03b5540ff06de09ec94cb71e77e1bd0015f9031
Goooler <wangzongler@gmail.com>
2022-03-17 15:34:31 +0800

    Merge custom observers

--- test/src/main/kotlin/io/goooler/demoapp/test/network/exception/CustomObservers.kt
+++ test/src/main/kotlin/io/goooler/demoapp/test/network/exception/CustomObservers.kt
@@ -2,6 +2,14 @@ package io.goooler.demoapp.test.network.exception
 
 import io.goooler.demoapp.common.network.BaseResponse
 import io.reactivex.rxjava3.observers.DisposableObserver
+import io.reactivex.rxjava3.observers.DisposableSingleObserver
+
+sealed interface CustomObserver<T : Any> {
+
+  fun onCustomSuccess(t: T)
+
+  fun onCustomError(e: ResponseException)
+}
 
 abstract class CustomObservableObserver<T : BaseResponse> :
   DisposableObserver<T>(),
@@ -19,4 +27,20 @@ abstract class CustomObservableObserver<T : BaseResponse> :
   }
 
   override fun onComplete() {}
+}
+
+abstract class CustomSingleObserver<T : BaseResponse> :
+  DisposableSingleObserver<T>(),
+  CustomObserver<T> {
+
+  override fun onSuccess(t: T) {
+    if (t.code == 200)
+      onCustomSuccess(t)
+    else
+      onCustomError((t.message ?: t.code.toString()).toResponseException())
+  }
+
+  override fun onError(e: Throwable) {
+    onCustomError(e.toResponseException())
+  }
 }
