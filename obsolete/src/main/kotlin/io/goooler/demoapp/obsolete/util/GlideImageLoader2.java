409985ab72019d0a538af4a62f87b8e5a22eb274
WangXiaoYang <wxygr11@163.com>
2022-04-13 15:48:19 +0800

    GlideImageLoader2 中的方法添加 static 关键字

--- obsolete/src/main/kotlin/io/goooler/demoapp/obsolete/util/GlideImageLoader2.java
+++ obsolete/src/main/kotlin/io/goooler/demoapp/obsolete/util/GlideImageLoader2.java
@@ -6,67 +6,67 @@ import androidx.annotation.FloatRange;
 
 class GlideImageLoader2 {
 
-  public void load(ImageView imageView, String url) {
+  public static void load(ImageView imageView, String url) {
     GlideImageLoader.load(imageView, url, null, null, 0, true);
   }
 
-  public void load(ImageView imageView, String url, @FloatRange(from = 0.0) Float cornerRadius) {
+  public static void load(ImageView imageView, String url, @FloatRange(from = 0.0) Float cornerRadius) {
     GlideImageLoader.load(imageView, url, null, null, cornerRadius.intValue(), true);
   }
 
-  public void load(ImageView imageView, String url, Drawable placeholderDrawable) {
+  public static void load(ImageView imageView, String url, Drawable placeholderDrawable) {
     GlideImageLoader.load(imageView, url, placeholderDrawable, null, 0, true);
   }
 
-  public void load(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable) {
+  public static void load(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable) {
     GlideImageLoader.load(imageView, url, placeholderDrawable, errorDrawable, 0, true);
   }
 
-  public void load(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable, @FloatRange(from = 0.0) Float cornerRadius) {
+  public static void load(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable, @FloatRange(from = 0.0) Float cornerRadius) {
     GlideImageLoader.load(imageView, url, placeholderDrawable, errorDrawable, cornerRadius.intValue(), true);
   }
 
-  public void load(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable, @FloatRange(from = 0.0) Float cornerRadius, Boolean useCache) {
+  public static void load(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable, @FloatRange(from = 0.0) Float cornerRadius, Boolean useCache) {
     GlideImageLoader.load(imageView, url, placeholderDrawable, errorDrawable, cornerRadius.intValue(), useCache);
   }
 
-  public void loadCircleCrop(ImageView imageView, String url) {
+  public static void loadCircleCrop(ImageView imageView, String url) {
     GlideImageLoader.loadCircleCrop(imageView, url, null, null, true);
   }
 
-  public void loadCircleCrop(ImageView imageView, String url, Drawable placeholderDrawable) {
+  public static void loadCircleCrop(ImageView imageView, String url, Drawable placeholderDrawable) {
     GlideImageLoader.loadCircleCrop(imageView, url, placeholderDrawable, null, true);
   }
 
-  public void loadCircleCrop(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable) {
+  public static void loadCircleCrop(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable) {
     GlideImageLoader.loadCircleCrop(imageView, url, placeholderDrawable, errorDrawable, true);
   }
 
-  public void loadCircleCrop(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable, Boolean useCache) {
+  public static void loadCircleCrop(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable, Boolean useCache) {
     GlideImageLoader.loadCircleCrop(imageView, url, placeholderDrawable, errorDrawable, useCache);
   }
 
-  public void loadCenterCrop(ImageView imageView, String url) {
+  public static void loadCenterCrop(ImageView imageView, String url) {
     GlideImageLoader.loadCenterCrop(imageView, url, null, null, 0, true);
   }
 
-  public void loadCenterCrop(ImageView imageView, String url, @FloatRange(from = 0.0) Float cornerRadius) {
+  public static void loadCenterCrop(ImageView imageView, String url, @FloatRange(from = 0.0) Float cornerRadius) {
     GlideImageLoader.loadCenterCrop(imageView, url, null, null, cornerRadius.intValue(), true);
   }
 
-  public void loadCenterCrop(ImageView imageView, String url, Drawable placeholderDrawable) {
+  public static void loadCenterCrop(ImageView imageView, String url, Drawable placeholderDrawable) {
     GlideImageLoader.loadCenterCrop(imageView, url, placeholderDrawable, null, 0, true);
   }
 
-  public void loadCenterCrop(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable) {
+  public static void loadCenterCrop(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable) {
     GlideImageLoader.loadCenterCrop(imageView, url, placeholderDrawable, errorDrawable, 0, true);
   }
 
-  public void loadCenterCrop(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable, @FloatRange(from = 0.0) Float cornerRadius) {
+  public static void loadCenterCrop(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable, @FloatRange(from = 0.0) Float cornerRadius) {
     GlideImageLoader.loadCenterCrop(imageView, url, placeholderDrawable, errorDrawable, cornerRadius.intValue(), true);
   }
 
-  public void loadCenterCrop(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable, @FloatRange(from = 0.0) Float cornerRadius, Boolean useCache) {
+  public static void loadCenterCrop(ImageView imageView, String url, Drawable placeholderDrawable, Drawable errorDrawable, @FloatRange(from = 0.0) Float cornerRadius, Boolean useCache) {
     GlideImageLoader.loadCenterCrop(imageView, url, placeholderDrawable, errorDrawable, cornerRadius.intValue(), useCache);
   }
 }
