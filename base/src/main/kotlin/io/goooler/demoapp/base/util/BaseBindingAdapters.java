dacfb6d801e47bf6936d4d6e34cb43bec4ea6f54
Goooler <wangzongler@gmail.com>
2022-04-14 16:28:45 +0800

    Use view extensions

--- base/src/main/kotlin/io/goooler/demoapp/base/util/BaseBindingAdapters.java
+++ base/src/main/kotlin/io/goooler/demoapp/base/util/BaseBindingAdapters.java
@@ -66,22 +66,22 @@ public interface BaseBindingAdapters {
 
   @BindingAdapter("binding_marginTop")
   static void bindingMarginTop(@NonNull View view, float margin) {
-    marginDirection(view, 1, margin);
+    BaseExtensionUtil.marginDirection(view, 1, margin);
   }
 
   @BindingAdapter("binding_marginBottom")
   static void bindingMarginBottom(@NonNull View view, float margin) {
-    marginDirection(view, 3, margin);
+    BaseExtensionUtil.marginDirection(view, 3, margin);
   }
 
   @BindingAdapter("binding_marginStart")
   static void bindingMarginStart(@NonNull View view, float margin) {
-    marginDirection(view, 0, margin);
+    BaseExtensionUtil.marginDirection(view, 0, margin);
   }
 
   @BindingAdapter("binding_marginEnd")
   static void bindingMarginEnd(@NonNull View view, float margin) {
-    marginDirection(view, 2, margin);
+    BaseExtensionUtil.marginDirection(view, 2, margin);
   }
 
 // ------------------------View Bg Shape---------------------//
@@ -99,7 +99,7 @@ public interface BaseBindingAdapters {
     int angle,
     @Px float radius
   ) {
-    setBgShapeGradual(
+    BaseExtensionUtil.setBgShapeGradual(
       view,
       GradientDrawable.RECTANGLE,
       new int[]{startColor, endColor},
@@ -127,7 +127,7 @@ public interface BaseBindingAdapters {
     @ColorInt int strokeColor,
     @Px float radius
   ) {
-    setBgShapeGradual(
+    BaseExtensionUtil.setBgShapeGradual(
       view,
       GradientDrawable.RECTANGLE,
       new int[]{startColor, endColor},
@@ -154,7 +154,7 @@ public interface BaseBindingAdapters {
     int angle,
     @Px float radius
   ) {
-    setBgShapeGradual(
+    BaseExtensionUtil.setBgShapeGradual(
       view,
       GradientDrawable.RECTANGLE,
       new int[]{startColor, centerColor, endColor},
@@ -187,7 +187,7 @@ public interface BaseBindingAdapters {
     @Px float bottomLeft,
     @Px float bottomRight
   ) {
-    setBgShapeCorners(view, solidColor, topLeft, topRight, bottomLeft, bottomRight);
+    BaseExtensionUtil.setBgShapeCorners(view, solidColor, topLeft, topRight, bottomLeft, bottomRight);
   }
 
   @BindingAdapter({
@@ -201,7 +201,7 @@ public interface BaseBindingAdapters {
     @ColorInt int endColor,
     int angle
   ) {
-    setBgShapeGradual(view, GradientDrawable.RECTANGLE, new int[]{startColor, endColor}, angle, null, Color.TRANSPARENT, 0f, 0f);
+    BaseExtensionUtil.setBgShapeGradual(view, GradientDrawable.RECTANGLE, new int[]{startColor, endColor}, angle, null, Color.TRANSPARENT, 0f, 0f);
   }
 
   @BindingAdapter({
@@ -217,7 +217,7 @@ public interface BaseBindingAdapters {
     @ColorInt int endColor,
     int angle
   ) {
-    setBgShapeGradual(view, GradientDrawable.RECTANGLE, new int[]{startColor, centerColor, endColor}, angle, null, Color.TRANSPARENT, 0f, 0f);
+    BaseExtensionUtil.setBgShapeGradual(view, GradientDrawable.RECTANGLE, new int[]{startColor, centerColor, endColor}, angle, null, Color.TRANSPARENT, 0f, 0f);
   }
 
   @BindingAdapter({
@@ -227,7 +227,7 @@ public interface BaseBindingAdapters {
     "binding_bg_radius"
   })
   static void bindingBgShapeStroke(@NonNull View view, @Px float stroke, @ColorInt int strokeColor, @ColorInt int solidColor, @Px float radius) {
-    setBgShapeGradual(view, GradientDrawable.RECTANGLE, null, 0, solidColor, strokeColor, stroke, radius);
+    BaseExtensionUtil.setBgShapeGradual(view, GradientDrawable.RECTANGLE, null, 0, solidColor, strokeColor, stroke, radius);
   }
 
   @BindingAdapter({
@@ -235,7 +235,7 @@ public interface BaseBindingAdapters {
     "binding_bg_radius"
   })
   static void bindingBgShape(@NonNull View view, @ColorInt int solidColor, @Px float radius) {
-    setBgShapeGradual(view, GradientDrawable.RECTANGLE, null, 0, solidColor, Color.TRANSPARENT, 0f, radius);
+    BaseExtensionUtil.setBgShapeGradual(view, GradientDrawable.RECTANGLE, null, 0, solidColor, Color.TRANSPARENT, 0f, radius);
   }
 
   @BindingAdapter({
@@ -244,12 +244,12 @@ public interface BaseBindingAdapters {
     "binding_bg_solidOvalColor"
   })
   static void bindingBgShapeOvalStroke(@NonNull View view, @Px float stroke, @ColorInt int strokeColor, @ColorInt int solidOvalColor) {
-    setBgShapeGradual(view, GradientDrawable.OVAL, null, 0, solidOvalColor, strokeColor, stroke, 0f);
+    BaseExtensionUtil.setBgShapeGradual(view, GradientDrawable.OVAL, null, 0, solidOvalColor, strokeColor, stroke, 0f);
   }
 
   @BindingAdapter("binding_bg_solidOvalColor")
   static void bindingBgShapeOval(@NonNull View view, @ColorInt int solidOvalColor) {
-    setBgShapeGradual(view, GradientDrawable.OVAL, null, 0, solidOvalColor, Color.TRANSPARENT, 0f, 0f);
+    BaseExtensionUtil.setBgShapeGradual(view, GradientDrawable.OVAL, null, 0, solidOvalColor, Color.TRANSPARENT, 0f, 0f);
   }
 
 // ------------------------TextView--------------------------//
@@ -272,123 +272,6 @@ public interface BaseBindingAdapters {
       textView.getPaint().setAntiAlias(true);
     } else {
       textView.getPaint().setFlags(0);
-    }
-  }
-
-// ---------------------Private-------------------------------//
-
-  /**
-   * 设置 view 的背景，支持圆形和矩形，渐变色和描边圆角等
-   *
-   * @param shapeType     背景形状，圆、矩形等
-   * @param gradualColors 渐变色数组，和填充色互斥
-   * @param angle         渐变色角度
-   * @param solidColor    填充色，和渐变色数组互斥
-   * @param strokeColor   描边色
-   * @param stroke        描边粗细
-   * @param radius        圆角大小
-   */
-  static void setBgShapeGradual(
-    @NonNull View view,
-    int shapeType,
-    @Nullable @ColorInt int[] gradualColors,
-    int angle,
-    @ColorInt Integer solidColor,
-    @ColorInt int strokeColor,
-    @Px float stroke,
-    @Px float radius
-  ) {
-    GradientDrawable gradientDrawable = new GradientDrawable();
-    gradientDrawable.setShape(shapeType);
-    gradientDrawable.setUseLevel(false);
-    gradientDrawable.setGradientType(GradientDrawable.LINEAR_GRADIENT);
-    int remainder = angle % 45;
-    int validAngle;
-    if (remainder >= 22.5) {
-      validAngle = angle % 360 + 45 - remainder;
-    } else {
-      validAngle = angle % 360 - remainder;
-    }
-    switch (validAngle) {
-      case 45:
-        gradientDrawable.setOrientation(GradientDrawable.Orientation.BL_TR);
-        break;
-      case 90:
-        gradientDrawable.setOrientation(GradientDrawable.Orientation.BOTTOM_TOP);
-        break;
-      case 135:
-        gradientDrawable.setOrientation(GradientDrawable.Orientation.BR_TL);
-        break;
-      case 180:
-        gradientDrawable.setOrientation(GradientDrawable.Orientation.RIGHT_LEFT);
-        break;
-      case 225:
-        gradientDrawable.setOrientation(GradientDrawable.Orientation.TR_BL);
-        break;
-      case 270:
-        gradientDrawable.setOrientation(GradientDrawable.Orientation.TOP_BOTTOM);
-        break;
-      case 315:
-        gradientDrawable.setOrientation(GradientDrawable.Orientation.TL_BR);
-        break;
-      default:
-        gradientDrawable.setOrientation(GradientDrawable.Orientation.LEFT_RIGHT);
-        break;
-    }
-
-    if (gradualColors != null && solidColor == null) {
-      gradientDrawable.setColors(gradualColors);
-    } else if (gradualColors == null && solidColor != null) {
-      gradientDrawable.setColor(solidColor);
-    }
-    gradientDrawable.setStroke((int) stroke, (int) strokeColor);
-    gradientDrawable.setCornerRadius(radius);
-    view.setBackground(gradientDrawable);
-  }
-
-  /**
-   * 设置 view 在矩形某几个角上需要圆角的背景
-   *
-   * @param solidColor  填充色
-   * @param topLeft     左上圆角大小
-   * @param topRight    右上圆角大小
-   * @param bottomLeft  左下圆角大小
-   * @param bottomRight 左下圆角大小
-   */
-  static void setBgShapeCorners(
-    @NonNull View view,
-    @ColorInt int solidColor,
-    @Px float topLeft,
-    @Px float topRight,
-    @Px float bottomLeft,
-    @Px float bottomRight
-  ) {
-    GradientDrawable gradientDrawable = new GradientDrawable();
-    gradientDrawable.setShape(GradientDrawable.RECTANGLE);
-    gradientDrawable.setColor(solidColor);
-    float[] radii = {topLeft, topLeft, topRight, topRight, bottomRight, bottomRight, bottomLeft, bottomLeft};
-    gradientDrawable.setCornerRadii(radii);
-    view.setBackground(gradientDrawable);
-  }
-
-  static void marginDirection(@NonNull View view, int direction, @Px float margin) {
-    if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
-      ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
-      switch (direction) {
-        case 0:
-          p.setMarginStart((int) margin);
-          break;
-        case 1:
-          p.topMargin = (int) margin;
-          break;
-        case 2:
-          p.setMarginEnd((int) margin);
-          break;
-        default:
-          p.bottomMargin = (int) margin;
-          break;
-      }
-      view.setLayoutParams(p);
     }
   }
 }
