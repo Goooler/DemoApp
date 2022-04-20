3829f2fa979152bb8c4e3d395fc3af1643e6e658
Goooler <wangzongler@gmail.com>
2022-04-20 16:30:31 +0800

    Apply dokka plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.kapt) apply false
+  alias(libs.plugins.dokka) apply false
   alias(libs.plugins.hilt) apply false
   alias(libs.plugins.ktlint) apply false
   alias(libs.plugins.moshiX) apply false
