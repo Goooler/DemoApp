30e6b3c4d74ab6698cfd13b2bd9910f2904882c4
Goooler <wangzongler@gmail.com>
2022-04-25 00:18:46 +0800

    Revert "Apply parcelize plugin"
    This reverts commit aa093e676b4c1fe293fbd9708ef1081388eb48d6.

--- biz/main/build.gradle.kts
+++ biz/main/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
   alias(libs.plugins.android.library)
   alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.kapt)
-  alias(libs.plugins.kotlin.parcelize)
   alias(libs.plugins.hilt)
   alias(libs.plugins.moshiX)
 }
