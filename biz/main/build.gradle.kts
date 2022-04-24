aa093e676b4c1fe293fbd9708ef1081388eb48d6
Goooler <wangzongler@gmail.com>
2022-04-24 20:59:21 +0800

    Apply parcelize plugin

--- biz/main/build.gradle.kts
+++ biz/main/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   alias(libs.plugins.android.library)
   alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.kapt)
+  alias(libs.plugins.kotlin.parcelize)
   alias(libs.plugins.hilt)
   alias(libs.plugins.moshiX)
 }
