c67e037f5c4c961da5056946e586981ecca68298
Goooler <wangzongler@gmail.com>
2022-08-24 23:01:21 +0800

    Use buildCache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,6 +28,15 @@ gradleEnterprise {
   }
 }
 
+val isCiBuild = System.getenv("GITHUB_ACTIONS") == "true"
+
+buildCache {
+  remote(HttpBuildCache::class.java) {
+    setUrl("https://gradle.com/terms-of-service/cache/")
+    isPush = isCiBuild
+  }
+}
+
 include(
   // App
   ":app",
