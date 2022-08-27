5d6ff27407ead97e8c4efbb45ad70bf0fe9f2239
Goooler <wangzongler@gmail.com>
2022-08-27 20:59:18 +0800

    Fix format

--- base/src/main/kotlin/io/goooler/demoapp/base/util/BaseExtensions.kt
+++ base/src/main/kotlin/io/goooler/demoapp/base/util/BaseExtensions.kt
@@ -57,13 +57,6 @@ import java.util.UUID
 import java.util.regex.Pattern
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.async
-import kotlinx.coroutines.withContext
 
 // ---------------------Types-------------------------------//
 
@@ -401,8 +394,6 @@ fun <T> MutableCollection<T>.removeFirstOrNull(predicate: (T) -> Boolean): T? {
 // ---------------------File-------------------------------//
 
 fun File.notExists(): Boolean = exists().not()
-
-// ---------------------View-------------------------------//
 
 // ---------------------Intent-------------------------------//
 
