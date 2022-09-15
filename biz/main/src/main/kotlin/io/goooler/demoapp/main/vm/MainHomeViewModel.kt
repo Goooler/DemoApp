e8607bf321660ed997ce2672dea551e44ddd3e35
Goooler <wangzongler@gmail.com>
2022-09-15 18:55:38 +0800

    print error in querying db

--- biz/main/src/main/kotlin/io/goooler/demoapp/main/vm/MainHomeViewModel.kt
+++ biz/main/src/main/kotlin/io/goooler/demoapp/main/vm/MainHomeViewModel.kt
@@ -71,7 +71,9 @@ class MainHomeViewModel : BaseViewModel() {
         val microsoft = async(SupervisorJob()) { repository.getRepoListFromDb("microsoft") }
 
         _title.value = processList(google.await(), microsoft.await())
-      } catch (_: Exception) {
+      } catch (e: Exception) {
+        _title.value = e.message.orEmpty()
+        e.printStackTrace()
       }
 
       try {
@@ -82,7 +84,6 @@ class MainHomeViewModel : BaseViewModel() {
 
         putRepoListIntoDb(google.await(), microsoft.await())
       } catch (e: Exception) {
-        io.goooler.demoapp.common.R.string.common_request_failed.showToast()
         _title.value = e.message.orEmpty()
         e.printStackTrace()
       }
