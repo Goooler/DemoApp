5d9a093c14c6f3de02b75eb6d56328c4ccad052d
Goooler <wangzongler@gmail.com>
2022-09-23 18:47:49 +0800

    Rearrange

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin.android) apply false
-  alias(libs.plugins.napt) apply false
   alias(libs.plugins.ksp) apply false
+  alias(libs.plugins.napt) apply false
   alias(libs.plugins.kotlinter) apply false
   alias(libs.plugins.detekt) apply false
 }
