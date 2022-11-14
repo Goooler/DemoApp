fb82a1f64058a053f8b7aa0340fd4d3fffb41bf6
Goooler <wangzongler@gmail.com>
2022-11-14 14:51:26 +0800

    Target sdk 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
   defaultConfig {
     applicationId = namespace
-    targetSdk = 33
+    targetSdk = 30
     versionCode = libs.versions.versionCode.get().toInt()
     versionName = libs.versions.versionName.get()
     resourceConfigurations += setOf("en", "zh-rCN", "xxhdpi")
