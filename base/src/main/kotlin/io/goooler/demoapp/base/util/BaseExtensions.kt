27aa6d4811fbe538172fc39c29eed9170c4fce59
Goooler <wangzongler@gmail.com>
2022-12-04 17:04:03 +0800

    Cleanup AudioPlayService (#281)
    Separated from #280.

--- base/src/main/kotlin/io/goooler/demoapp/base/util/BaseExtensions.kt
+++ base/src/main/kotlin/io/goooler/demoapp/base/util/BaseExtensions.kt
@@ -531,6 +531,8 @@ fun Context.setMusicMute(mute: Boolean = true) {
   }
 }
 
+inline fun <reified T : Any> Context.requireSystemService(): T = checkNotNull(getSystemService())
+
 // ---------------------Activity-------------------------------//
 
 @MainThread
