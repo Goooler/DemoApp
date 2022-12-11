8b71bae9c5cda1fefc789efb47c7478c4c3d7ab9
Goooler <wangzongler@gmail.com>
2022-12-11 19:03:09 +0800

    Use kapt

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.ksp) apply false
-  alias(libs.plugins.napt) apply false
+  alias(libs.plugins.kotlin.kapt) apply false
   alias(libs.plugins.kotlinter) apply false
   alias(libs.plugins.detekt) apply false
 }
