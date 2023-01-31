dd3bef7fd9998d5b92de280d544912bfc45e36b3
Goooler <wangzongler@gmail.com>
2023-01-31 10:23:39 +0800

    Use `RewriteQueriesToDropUnusedColumns`
    https://github.com/Goooler/tivi/commit/203042b627eaa90b1cf8476f13c731e920eb8bd2
    https://developer.android.com/reference/androidx/room/RewriteQueriesToDropUnusedColumns

--- biz/main/src/main/kotlin/io/goooler/demoapp/main/db/MainCommonDao.kt
+++ biz/main/src/main/kotlin/io/goooler/demoapp/main/db/MainCommonDao.kt
@@ -4,11 +4,13 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.RewriteQueriesToDropUnusedColumns
 import io.goooler.demoapp.main.bean.MainRepoListBean
 
 @Dao
 interface MainCommonDao {
 
+  @RewriteQueriesToDropUnusedColumns
   @Query("SELECT * FROM main_repo_list WHERE owner_name = :ownerName")
   suspend fun getRepoList(ownerName: String): List<MainRepoListBean>
 
