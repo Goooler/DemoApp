a29c228b892f260ec8cd8410515d5212e06d5a20
Goooler <wangzongler@gmail.com>
2023-02-16 01:56:54 +0800

    Migrate to Room's Upsert (#340)
    https://developer.android.com/reference/kotlin/androidx/room/Upsert
    https://github.com/chrisbanes/tivi/commit/aae4a87cc44986fa417c7eef43a5c2fa973fbe93

--- biz/main/src/main/kotlin/io/goooler/demoapp/main/db/MainCommonDao.kt
+++ biz/main/src/main/kotlin/io/goooler/demoapp/main/db/MainCommonDao.kt
@@ -1,10 +1,9 @@
 package io.goooler.demoapp.main.db
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.RewriteQueriesToDropUnusedColumns
+import androidx.room.Upsert
 import io.goooler.demoapp.main.bean.MainRepoListBean
 
 @Dao
@@ -14,6 +13,6 @@ interface MainCommonDao {
   @Query("SELECT * FROM main_repo_list WHERE owner_name = :ownerName")
   suspend fun getRepoList(ownerName: String): List<MainRepoListBean>
 
-  @Insert(onConflict = OnConflictStrategy.REPLACE)
-  suspend fun insertRepoList(list: List<MainRepoListBean>)
+  @Upsert
+  suspend fun upsertRepoList(list: List<MainRepoListBean>)
 }
