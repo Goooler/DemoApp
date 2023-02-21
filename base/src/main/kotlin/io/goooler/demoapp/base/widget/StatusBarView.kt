ad8e386c51433ac0c27d5fa3267f9d6c21618c05
Goooler <wangzongler@gmail.com>
2023-02-21 11:00:46 +0800

    Init final field in TypedArray.use

--- base/src/main/kotlin/io/goooler/demoapp/base/widget/StatusBarView.kt
+++ base/src/main/kotlin/io/goooler/demoapp/base/widget/StatusBarView.kt
@@ -7,8 +7,19 @@ import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.DimenRes
 import androidx.annotation.Px
+import androidx.core.content.res.use
 
 class StatusBarView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+  private val height: Int
+
+  init {
+    context.obtainStyledAttributes(
+      attrs,
+      androidx.appcompat.R.styleable.View
+    ).use {
+      height = it.getDimension(androidx.appcompat.R.styleable.ActionBar_height, 0F).toInt()
+    }
+  }
 
   override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
     setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), getStatusBarHeight())
