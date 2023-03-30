90cb05d26b041f564c5d307748549e0e614ede40
Goooler <wangzongler@gmail.com>
2023-03-30 12:12:24 +0800

    Filter all supers' binding type arguments (#368)
    Follow up fa39546a49d3e45e722a92830d8d622f3d44d4bc.

--- common/src/main/kotlin/io/goooler/demoapp/common/base/binding/IBinding.kt
+++ common/src/main/kotlin/io/goooler/demoapp/common/base/binding/IBinding.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import androidx.annotation.MainThread
 import androidx.databinding.ViewDataBinding
 import androidx.viewbinding.ViewBinding
+import java.lang.reflect.Method
 import java.lang.reflect.ParameterizedType
 
 internal sealed interface IBinding<VB : ViewDataBinding> {
@@ -12,13 +13,26 @@ internal sealed interface IBinding<VB : ViewDataBinding> {
   companion object {
     @Suppress("UNCHECKED_CAST")
     internal fun <T : ViewBinding> IBinding<*>.inflateBinding(inflater: LayoutInflater): T {
-      return (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments
-        .asSequence()
-        .filterIsInstance<Class<T>>()
-        .first { it.simpleName.endsWith("Binding") }
-        .getDeclaredMethod("inflate", LayoutInflater::class.java)
-        .also { it.isAccessible = true }
-        .invoke(null, inflater) as T
+      var method: Method?
+      var clazz: Class<*> = javaClass
+      while (clazz.superclass != null) {
+        method = clazz.filterBindingMethod()
+        if (method == null) {
+          clazz = clazz.superclass
+        } else {
+          return method.invoke(null, inflater) as T
+        }
+      }
+      error("No Binding type argument found.")
+    }
+
+    private fun Class<*>.filterBindingMethod(): Method? {
+      return (genericSuperclass as? ParameterizedType)?.actualTypeArguments
+        ?.asSequence()
+        ?.filterIsInstance<Class<*>>()
+        ?.firstOrNull { it.simpleName.endsWith("Binding") }
+        ?.getDeclaredMethod("inflate", LayoutInflater::class.java)
+        ?.also { it.isAccessible = true }
     }
   }
 }
