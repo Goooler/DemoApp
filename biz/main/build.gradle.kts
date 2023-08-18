ed07d8f74600dec5e0fb8d6ebf3a212bbb024c76
Goooler <wangzongler@gmail.com>
2023-08-18 09:53:34 +0800

    com.github.limbowangqi:FloatWindow

--- biz/main/build.gradle.kts
+++ biz/main/build.gradle.kts
@@ -25,5 +25,5 @@ dependencies {
   implementation(libs.square.moshi)
   ksp(libs.square.moshi.compiler)
 
-  implementation("com.github.limbowangqi:FloatWindow:1.0.1")
+  implementation("com.github.Petterpx:FloatingX:1.3.1")
 }
