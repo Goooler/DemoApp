9620acbac1f978bf34a58849b377db3bf3eee8ea
Goooler <wangzongler@gmail.com>
2023-08-24 21:24:54 +0800

    Remove compileOptions

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,11 +113,6 @@ fun <T : BaseExtension> Project.setupBase(block: T.() -> Unit) {
     sourceSets.configureEach {
       java.srcDirs("src/$name/kotlin")
     }
-    // Can remove this once https://issuetracker.google.com/issues/260059413 is fixed.
-    compileOptions {
-      sourceCompatibility = JavaVersion.VERSION_17
-      targetCompatibility = JavaVersion.VERSION_17
-    }
     packagingOptions.resources.excludes += setOf(
       "**/*.proto",
       "**/*.bin",
