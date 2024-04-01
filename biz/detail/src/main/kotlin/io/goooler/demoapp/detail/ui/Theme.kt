ab543b0546d836735da5ec80af881acc929b9ed4
Goooler <wangzongler@gmail.com>
2024-04-01 15:51:16 +0800

    Add PaddingValues.copy

--- biz/detail/src/main/kotlin/io/goooler/demoapp/detail/ui/Theme.kt
+++ biz/detail/src/main/kotlin/io/goooler/demoapp/detail/ui/Theme.kt
@@ -3,6 +3,8 @@ package io.goooler.demoapp.detail.ui
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -13,6 +15,9 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 
 private val DarkColorScheme = darkColorScheme()
 
@@ -64,3 +69,17 @@ fun DemoScaffold(
     )
   }
 }
+
+@Composable
+fun PaddingValues.copy(
+  layoutDirection: LayoutDirection = LocalLayoutDirection.current,
+  start: Dp = calculateStartPadding(layoutDirection),
+  top: Dp = calculateTopPadding(),
+  end: Dp = calculateEndPadding(layoutDirection),
+  bottom: Dp = calculateBottomPadding(),
+) = PaddingValues(
+  start = start,
+  top = top,
+  end = end,
+  bottom = bottom,
+)
