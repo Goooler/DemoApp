e4b069fef681b792991b32084a542b6f4ceb6a05
Goooler <wangzongler@gmail.com>
2024-06-24 10:39:33 +0800

    Better error message

--- adapter/src/main/kotlin/io/goooler/demoapp/adapter/rv/core/IRvAdapterDelegate.kt
+++ adapter/src/main/kotlin/io/goooler/demoapp/adapter/rv/core/IRvAdapterDelegate.kt
@@ -121,11 +121,11 @@ internal class RvAdapterDelegate<M : IVhModelType> : IRvAdapterDelegate<M, Bindi
   }
 
   override fun onCreateVHForAll(binding: ViewDataBinding) {
-    error("Shouldn't call this in delegate.")
+    error("Should override this in your adapter.")
   }
 
   override fun onBindVHForAll(binding: ViewDataBinding, model: M, payloads: List<Any>) {
-    error("Shouldn't call this in delegate.")
+    error("Should override this in your adapter.")
   }
 
   /**
