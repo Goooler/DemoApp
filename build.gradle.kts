ae981f8a1c4d25f7cbe865d8dd2248cd8fac0d03
Goooler <wangzongler@gmail.com>
2024-06-26 10:39:19 +0800

    Apply BCV

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.bcv) apply false
   alias(libs.plugins.ksp) apply false
   alias(libs.plugins.napt) apply false
   alias(libs.plugins.spotless) apply false
