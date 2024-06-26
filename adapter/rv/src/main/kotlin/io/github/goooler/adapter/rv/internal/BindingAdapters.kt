2ebb5f65771666c0b67b94cd148e5cad1a74d7f9
Goooler <wangzongler@gmail.com>
2024-06-26 15:41:23 +0800

    Suppress UNCHECKED_CAST for BindingAdapters

--- adapter/rv/src/main/kotlin/io/github/goooler/adapter/rv/internal/BindingAdapters.kt
+++ adapter/rv/src/main/kotlin/io/github/goooler/adapter/rv/internal/BindingAdapters.kt
@@ -7,18 +7,17 @@ import io.github.goooler.adapter.rv.core.IVhModelType
 /**
  * Used for generating binding adapters, mark the class as internal to avoid using in Kotlin.
  */
+@Suppress("UNCHECKED_CAST")
 internal object BindingAdapters {
   @BindingAdapter("binding_rv_dataList")
   @JvmStatic
   fun <M : IVhModelType> RecyclerView.bindingSetList(list: List<M>?) {
-    @Suppress("UNCHECKED_CAST")
     (adapter as? IMutableRvAdapter<M>)?.list = list.orEmpty()
   }
 
   @BindingAdapter("binding_rv_refreshItems")
   @JvmStatic
   fun <M : IVhModelType> RecyclerView.bindingRefreshItems(vararg items: Triple<Int, M, Any?>) {
-    @Suppress("UNCHECKED_CAST")
     (adapter as? IMutableRvAdapter<M>)?.refreshItems(*items)
   }
 }
